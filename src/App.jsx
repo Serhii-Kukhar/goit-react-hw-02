c66c02d93fe1e53280c205422af24f9d816487aa
Serhii Kukhar <s3riykuhar1@gmail.com>
2025-03-23 17:27:35 +0100

    fixbug

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Description from "./components/Description/Description";
 import Options from "./components/Options/Options";
 import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
-// import "modern-normalize";
+import "modern-normalize";
 
 const App = () => {
   const [values, setValues] = useState({
