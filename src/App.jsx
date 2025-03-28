54798c53e8a96add59fad445a8c920b1d7b9a70e
Serhii Kukhar <s3riykuhar1@gmail.com>
2025-03-28 16:11:57 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Description from "./components/Description/Description";
 import Options from "./components/Options/Options";
 import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
-import "modern-normalize";
 
 const App = () => {
-  const [values, setValues] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [values, setValues] = useState(() => {
+    const savedValues = localStorage.getItem("values");
+    return savedValues ? JSON.parse(savedValues) : { good: 0, neutral: 0, bad: 0 };
   });
-  const totalFeedback = values.good + values.neutral + values.bad;
+
+  useEffect(() => {
+    localStorage.setItem("values", JSON.stringify(values));
+  }, [values]);
+
+
 
   const updateFeedback = (feedbackType) => {
-    setValues((prevValues) => ({
-      ...prevValues,
-      [feedbackType]: prevValues[feedbackType] + 1,
-    }));
+    setValues((prevValues) => {
+      const newValues = { ...prevValues, [feedbackType]: prevValues[feedbackType] + 1 };
+      localStorage.setItem("values", JSON.stringify(newValues));
+      return newValues;
+    });
   };
 
-  const resetFeedback = ()=>{
-    setValues({
-      good:0,
-      neutral:0,
-      bad:0
-    })
-  }
+  const resetFeedback = () => {
+    setValues({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalFeedback = values.good + values.neutral + values.bad;
+  const positiveFeedback = Math.round((values.good / totalFeedback) * 100);
+
 
   return (
     <>
       <Description />
       <Options updateFeedback={updateFeedback} resetFeedback={resetFeedback} totalFeedback={totalFeedback} />
-      {totalFeedback > 0 ? (<Feedback feedback={values} />) : <Notification />}
+      {totalFeedback > 0 ? <Feedback feedback={values} totalFeedback={totalFeedback} positiveFeedback={positiveFeedback} /> : <Notification />}
     </>
   );
 };
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const Feedback = ({ feedback }) => {
+const Feedback = ({ feedback,totalFeedback,positiveFeedback}) => {
   return (
     <div>
       <p>Good: {feedback.good}</p>
       <p>Neutral: {feedback.neutral}</p>
       <p>Bad: {feedback.bad}</p>
+      <p>Total: {totalFeedback}</p>
+      <p>Positive: {positiveFeedback}%</p>
     </div>
   );
 };
